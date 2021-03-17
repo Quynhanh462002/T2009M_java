80817c1a41569c1ad51d5ed433c8ce0c4725fcb8
Quynh anh <71160175+Quynhanh4622@users.noreply.github.com>
2021-03-17 20:56:12 +0700

    nop lab s1

---
+++ src/lab/s1/Main.java
@@ -0,0 +1,8 @@
+package lab.s1;
+
+public class Main {
+    public static void main(String[] args){
+        Product p = new Product();
+        p.run();
+    }
+}
---
+++ src/lab/s1/Product.java
@@ -0,0 +1,9 @@
+package lab.s1;
+
+public class Product {
+    public void run(){
+        System.out.println("Running...");
+    }
+
+
+}
